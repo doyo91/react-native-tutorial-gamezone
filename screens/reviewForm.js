03f769f485421f8c0f8bf6d528155638784b62cb
doyo91 <doyo_91@hotmail.com>
2020-11-16 14:13:09 +0100

    add shema with yup

--- screens/reviewForm.js
+++ screens/reviewForm.js
@@ -2,11 +2,24 @@ import React from "react";
 import { View, Text, Button, TextInput, StyleSheet } from "react-native";
 import { globalStyles } from "../styles/globalStyles";
 import { Formik } from "formik";
+import * as yup from "yup";
+
+const ReviewSchema = yup.object({
+  title: yup.string().required().min(4),
+  body: yup.string().required().min(8),
+  rating: yup
+    .string()
+    .required()
+    .test("is-num-1-5", "Rating must be a number 1 - 5", (val) => {
+      return parseInt(val) < 6 && parseInt(val) > 0;
+    }),
+});
 
 export const ReviewForm = ({ addReview }) => {
   return (
     <View style={globalStyles.container}>
       <Formik
+        validationSchema={ReviewSchema}
         initialValues={{ title: "", body: "", rating: "" }}
         onSubmit={(values, actions) => {
           actions.resetForm();
